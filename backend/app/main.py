2905cbae5a1cd45794a7c24c61da2c51a8e64a64
Yohan Markose <markose.y@northeastern.edu>
2025-01-27 14:29:01 -0500

    Added feature folder and updated the backend main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,11 @@
 from fastapi import FastAPI
-# from  models import Todo
-
-# app = FastAPI()
-
-# todos = []
-
-# @app.get("/todos")
-# async def get_todo():
-#     return {"todos": todos}
-
-# @app.post("/todos")
-# async def create_todo(todo: Todo):
-#     todos.append(todo)
-#     return {"message": "Todo has been added"}
 
 from fastapi import FastAPI, UploadFile, Form
 from pydantic import BaseModel
 import requests
 from bs4 import BeautifulSoup
 # import pdfplumber
-# import boto3
+import boto3
 # import markdown
 import os
 from features.web_extraction.datascraper import WikiSpider, scrape_url 
@@ -27,21 +13,22 @@ from features.web_extraction.datascraper import WikiSpider, scrape_url
 app = FastAPI()
 
 # AWS S3 Configurations
-# AWS_ACCESS_KEY_ID = "your_aws_access_key"
-# AWS_SECRET_ACCESS_KEY = "your_aws_secret_key"
-# AWS_BUCKET_NAME = "your_bucket_name"
-# s3_client = boto3.client(
-#     "s3",
-#     aws_access_key_id=AWS_ACCESS_KEY_ID,
-#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-# )
+AWS_ACCESS_KEY_ID = "AKIAYKFQQUN54HLOMPMN"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_BUCKET_NAME = "pdfparserdataset"
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+)
+
 
 class URLInput(BaseModel):
     url: str
 
 
-# def upload_to_s3(file_name, content):
-#     s3_client.put_object(Bucket=AWS_BUCKET_NAME, Key=file_name, Body=content)
+def upload_to_s3(file_name, content):
+    s3_client.put_object(Bucket=AWS_BUCKET_NAME, Key=file_name, Body=content)
 
 @app.post("/scrape-url")
 def process_url(url_input: URLInput):
@@ -51,6 +38,7 @@ def process_url(url_input: URLInput):
     # markdown_content = markdown.markdown(text)
     # result = scrape_url(url_input.url)  # Scrape the URL
     file_name = "scraped_url.md"
+    upload_to_s3(file_name, text)
 
     return {
         "message": f"File {file_name} ",
